b45ee165cc9f204498e51095ca55ea4b03bba2ee
Kelly Stuard <kellystuard@gmail.com>
2019-04-15 14:34:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 provider "azurerm" {
   version = "=1.24.0"
 }
+resource "azurerm_resource_group" "k8s" {
+  name     = "k8s"
+  location = "Central US"
+  
+  tags = {
+    environment = "var.environment"
+  }
+}
