6d3b0f7b5158f94ad30f5fe3934e114fa208abf3
Kelly Stuard <kellystuard@gmail.com>
2019-04-15 15:11:56 -0500

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,9 +1,36 @@
 data "azurerm_subscription" "primary" {}
 
-data "azurerm_client_config" "current" {}
+resource "azurerm_azuread_application" "aks_app" {
+  name = "aks_app"
+}
+
+resource "azurerm_azuread_service_principal" "aks_sp" {
+  application_id = "${azurerm_azuread_application.aks_app.application_id}"
+}
 
 resource "azurerm_role_assignment" "current_contributor" {
   scope                = "${data.azurerm_subscription.primary.id}"
   role_definition_name = "Contributor"
-  principal_id         = "${data.azurerm_client_config.current.service_principal_object_id}"
+  principal_id         = "${azurerm_azuread_service_principal.aks_sp.id}"
+}
+
+resource "random_string" "aks_sp_password" {
+  length  = 16
+  special = true
+
+  keepers = {
+    service_principal = "${azurerm_azuread_service_principal.aks_sp.id}"
+  }
+}
+
+resource "azurerm_azuread_service_principal_password" "aks_sp_password" {
+  service_principal_id = "${azurerm_azuread_service_principal.aks_sp.id}"
+  value                = "${random_string.aks_sp_password.result}"
+  end_date             = "${timeadd(timestamp(), "8760h")}"
+
+  # This stops be 'end_date' changing on each run and causing a new password to be set
+  # to get the date to change here you would have to manually taint this resource...
+  lifecycle {
+    ignore_changes = ["end_date"]
+  }
 }
