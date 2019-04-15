edf38bb50622ff8f7fe60a8448ae950a136e5fb3
Kelly Stuard <kellystuard@gmail.com>
2019-04-15 14:41:55 -0500

    Update security.tf

--- security.tf
+++ security.tf
@@ -5,5 +5,5 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_role_assignment" "current_contributor" {
   scope                = "${data.azurerm_subscription.primary.id}"
   role_definition_name = "Contributor"
-  principal_id         = "${data.azurerm_client_config.test.service_principal_object_id}"
+  principal_id         = "${data.azurerm_client_config.current.service_principal_object_id}"
 }
