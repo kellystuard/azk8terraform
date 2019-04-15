42630231f2308b216048065420b7aa48873f00d4
Kelly Stuard <kellystuard@gmail.com>
2019-04-15 15:25:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 terraform {
   required_version = "=0.11.13"
 }
+
+provider "random" {
+  version = "~> 2.1"
+}
+
 provider "azurerm" {
   version = "=1.24.0"
 }
+
 resource "azurerm_resource_group" "k8s" {
   name     = "k8s"
   location = "Central US"
