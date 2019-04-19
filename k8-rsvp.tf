2cc2d8032c7e593332cb235961738ffbb94e26e4
Kelly Stuard <kellystuard@gmail.com>
2019-04-19 14:17:28 -0500

    Create k8-rsvp.tf

---
+++ k8-rsvp.tf
@@ -0,0 +1,101 @@
+resource "kubernetes_deployment" "rsvp_db" {
+  metadata {
+    name      = "rsvp-db"
+    namespace = "testing-jsayar"
+  }
+  spec {
+    replicas = 1
+    template {
+      metadata {
+        labels {
+          appdb = "rsvpdb"
+        }
+      }
+      spec {
+        container {
+          name  = "rsvpd-db"
+          image = "mongo:3.3"
+          port {
+            container_port = 27017
+          }
+          env {
+            name  = "MONGODB_DATABASE"
+            value = "rsvpdata"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "mongodb" {
+  metadata {
+    name      = "mongodb"
+    namespace = "testing-jsayar"
+    labels {
+      app = "rsvpdb"
+    }
+  }
+  spec {
+    port {
+      protocol = "TCP"
+      port     = 27017
+    }
+    selector {
+      appdb = "rsvpdb"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "rsvp" {
+  metadata {
+    name      = "rsvp"
+    namespace = "testing-jsayar"
+  }
+  spec {
+    replicas = 1
+    template {
+      metadata {
+        labels {
+          app = "rsvp"
+        }
+      }
+      spec {
+        container {
+          name  = "rsvp-app"
+          image = "teamcloudyuga/rsvpapp"
+          port {
+            name           = "web-port"
+            container_port = 5000
+          }
+          env {
+            name  = "MONGODB_HOST"
+            value = "mongodb"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "rsvp" {
+  metadata {
+    name      = "rsvp"
+    namespace = "testing-jsayar"
+    labels {
+      apps = "rsvp"
+    }
+  }
+  spec {
+    port {
+      name      = "tcp-31081-5000"
+      protocol  = "TCP"
+      port      = 5000
+      node_port = 31081
+    }
+    selector {
+      app = "rsvp"
+    }
+    type = "NodePort"
+  }
+}
