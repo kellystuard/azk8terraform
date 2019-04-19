a70edf4c1b30700f958dbc4d2168cfd780684db7
Kelly Stuard <kellystuard@gmail.com>
2019-04-19 15:51:01 -0500

    Update kubernetes-rsvp.tf

--- kubernetes-rsvp.tf
+++ kubernetes-rsvp.tf
@@ -133,10 +133,11 @@ resource "kubernetes_service" "rsvp" {
     type             = "LoadBalancer"
     load_balancer_ip = "${azurerm_public_ip.k8s_rsvp.ip_address}"
     port {
-      name      = "tcp-31081-5000"
-      protocol  = "TCP"
-      port      = 5000
-      node_port = 31081
+      name        = "tcp-31081-5000"
+      protocol    = "TCP"
+      port        = 80
+      node_port   = 31081
+      target_port = 5000
     }
     selector {
       app = "rsvp"
