c52127eec6328debf959d35f08f5456d696c6233
Kelly Stuard <kellystuard@gmail.com>
2019-04-19 15:28:08 -0500

    Update kubernetes-rsvp.tf

--- kubernetes-rsvp.tf
+++ kubernetes-rsvp.tf
@@ -113,6 +113,14 @@ resource "kubernetes_deployment" "rsvp" {
   }
 }
 
+resource "azurerm_public_ip" "k8s_rsvp" {
+  name                         = "k8s_rsvp"
+  location                     = "${data.azurerm_resource_group.k8s.location}"
+  resource_group_name          = "${data.azurerm_resource_group.k8s.name}"
+  allocation_method            = "Static"
+  sku                          = "Basic"
+}
+
 resource "kubernetes_service" "rsvp" {
   metadata {
     name      = "rsvp"
@@ -131,6 +139,6 @@ resource "kubernetes_service" "rsvp" {
     selector {
       app = "rsvp"
     }
-    type = "NodePort"
+    type = "LoadBalancer"
   }
 }
