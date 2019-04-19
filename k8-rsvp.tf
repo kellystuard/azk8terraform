026e9438808e97ad689ff25bcd7d9d5b6cd1dc06
Kelly Stuard <kellystuard@gmail.com>
2019-04-19 14:27:19 -0500

    Update k8-rsvp.tf

--- k8-rsvp.tf
+++ k8-rsvp.tf
@@ -2,8 +2,6 @@ provider "kubernetes" {
   version                = "~> 1.6"
   
   host                   = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
-  username               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
-  password               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
   client_certificate     = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)}"
   client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)}"
   cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)}"
