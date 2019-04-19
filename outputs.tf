78e870e3b56c07563b3a522a43dee8068661f3ae
Kelly Stuard <kellystuard@gmail.com>
2019-04-19 15:54:08 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,6 @@ output "host" {
   value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
 }
 
-output "k8s_rsvp_ip" {
-  value = "${kubernetes_service.rsvp.load_balancer_ingress.ip}"
-}
-output "k8s_rsvp_hostname" {
-  value = "${kubernetes_service.rsvp.load_balancer_ingress.hostname}"
+output "k8s_rsvp" {
+  value = "${kubernetes_service.rsvp.load_balancer_ingress}"
 }
