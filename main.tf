180aaafff7f7899e49231d7c9403bfc9b6169c4b
Kelly Stuard <kellystuard@gmail.com>
2019-04-22 11:24:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,32 @@ resource "azurerm_resource_group" "k8s" {
     environment = "var.environment"
   }
 }
+
+resource "azurerm_virtual_network" "k8s" {
+  name                = "k8s"
+  location            = "${azurerm_resource_group.k8s.location}"
+  resource_group_name = "${azurerm_resource_group.k8s.name}"
+  address_space       = ["15.0.0.0/8"]
+}
+
+resource "azurerm_subnet" "kubesubnet" {
+  name                 = "kubesubnet"
+  virtual_network_name = "${azurerm_virtual_network.k8s.name}"
+  resource_group_name  = "${azurerm_resource_group.k8s.name}"
+  address_prefix       = "15.0.0.0/16" 
+}
+
+resource "azurerm_subnet" "appgwsubnet" {
+  name                 = "appgwsubnet"
+  virtual_network_name = "${azurerm_virtual_network.k8s.name}"
+  resource_group_name  = "${azurerm_resource_group.k8s.name}"
+  address_prefix       = "15.1.0.0/16"
+}
+
+resource "azurerm_public_ip" "k8s" {
+  name                = "k8s"
+  location            = "${azurerm_resource_group.k8s.location}"
+  resource_group_name = "${azurerm_resource_group.k8s.name}"
+  sku                 = "Basic"
+  allocation_method   = "Dynamic"
+}
