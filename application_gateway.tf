ff58da2641a119ab001707cd40c9fc1bf40a4ca5
Kelly Stuard <kellystuard@gmail.com>
2019-04-23 09:52:35 -0500

    Update application_gateway.tf

--- application_gateway.tf
+++ application_gateway.tf
@@ -41,7 +41,8 @@ resource "azurerm_application_gateway" "network" {
   }
 
   backend_address_pool {
-    name = "${local.backend_address_pool_name}"
+    name         = "${local.backend_address_pool_name}"
+    ip_addresses = ["${kubernetes_service.nginx.load_balancer_ingress.0.ip}"]
   }
 
   backend_http_settings {
