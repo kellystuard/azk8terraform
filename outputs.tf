ec070af7059fbc0cc1862c7059c0aa9452b4e8f5
Kelly Stuard <kellystuard@gmail.com>
2019-04-23 15:15:38 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "public_ip" {
-  value = "${azurerm_public_ip.k8s}"
+  value = "${azurerm_public_ip.k8s.ip_address}"
 }
 output "kube_config" {
   value     = "${azurerm_kubernetes_cluster.k8s.kube_config_raw}"
