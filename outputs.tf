654ac2dc25fe07ca9d864d2610766997da891529
Kelly Stuard <kellystuard@gmail.com>
2019-04-24 11:49:30 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "public_ip" {
   value = "${azurerm_public_ip.k8s.ip_address}"
 }
-output "kube_config" {
-  value     = "${azurerm_kubernetes_cluster.k8s.kube_config_raw}"
+output "k8s-ingress" {
+  value     = "${azurerm_kubernetes_cluster.k8s_ingress.kube_config_raw}"
   sensitive = true
 }
 
 output "host" {
-  value = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
+  value = "${azurerm_kubernetes_cluster.k8s_ingress.kube_config.0.host}"
 }
