f13087eb53c53e0c8d54595624b39ca016478400
Kelly Stuard <kellystuard@gmail.com>
2019-04-25 12:26:53 -0500

    Update helm-nginx.tf

--- helm-nginx.tf
+++ helm-nginx.tf
@@ -3,9 +3,9 @@ provider "helm" {
   
   kubernetes {
     host                   = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
-    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.k8s-ingress.kube_config.0.client_certificate)}"
-    client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s-ingress.kube_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s-ingress.kube_config.0.cluster_ca_certificate)}"
+    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.k8s_ingress.kube_config.0.client_certificate)}"
+    client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s_ingress.kube_config.0.client_key)}"
+    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s_ingress.kube_config.0.cluster_ca_certificate)}"
   }
 }
 
