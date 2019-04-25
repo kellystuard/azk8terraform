b71af193131345d4d1432ddcd5e6c3a1a1bc0932
Kelly Stuard <kellystuard@gmail.com>
2019-04-25 14:42:34 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "k8s_ingress" {
 
   agent_pool_profile {
     name            = "default"
-    count           = "4"
+    count           = "3"
     vm_size         = "Standard_B2s"
     os_type         = "Linux"
     #allowed range: 30-1024
