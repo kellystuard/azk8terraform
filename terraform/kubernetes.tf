0b706c99f9c59be6daea0c2475de4b2f3c49ac76
Kelly Stuard <kellystuard@gmail.com>
2019-04-26 14:17:13 -0500

    Update kubernetes.tf

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,5 +1,5 @@
 resource "azurerm_kubernetes_cluster" "k8s_ingress" {
-  name                = "azk8-ingress"
+  name                = "kubernetes-${var.environment}"
   location            = "${azurerm_resource_group.k8s.location}"
   resource_group_name = "${azurerm_resource_group.k8s.name}"
   dns_prefix          = "azk8-ingress-${var.environment}"
@@ -43,6 +43,6 @@ resource "azurerm_kubernetes_cluster" "k8s_ingress" {
   }
 
   tags {
-    Environment = "${var.environment}"
+    environment = "${var.environment}"
   }
 }
