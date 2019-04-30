526e2207a7b8d1ea2bc81c75007f08927d41db10
Kelly Stuard <kellystuard@gmail.com>
2019-04-30 13:14:36 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "environment" {
   description = "Name of environment. Used to tag resources."
   default     = "demo"
 }
+
+variable "subnet" {
+  type        = "string"
+  description = "Subnet of environment. Used to partition out the address space. Use a /16 space.
+  default     = "15.0.0.0/16"
+}
