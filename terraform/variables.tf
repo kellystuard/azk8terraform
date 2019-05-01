2eeb18fb6ea0be760b4388698c1dec2ad1be1140
Kelly Stuard <kellystuard@gmail.com>
2019-05-01 10:06:34 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,11 +4,15 @@ resource "random_pet" "environment" {
 variable "environment" {
   type        = "string"
   description = "Name of environment. Used to tag resources."
-  default     = "demo"
+  default     = ""
 }
 
 variable "subnet" {
   type        = "string"
   description = "Subnet of environment. Used to partition out the address space. Use a /16 space."
   default     = "15.0.0.0/16"
+}
+
+locals {
+  environment = "${var.environment == "" ? random_pet.environment.id : var.environment}"
 }
