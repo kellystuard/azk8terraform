8e8093290e2224077d9d340fc4c3c02583405d8d
Kelly Stuard <kellystuard@gmail.com>
2019-05-17 11:11:50 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_subnet" "k8s_lb" {
 resource "azurerm_public_ip" "k8s" {
   name                = "public-ip-${local.environment}"
   location            = "${azurerm_resource_group.k8s.location}"
-  resource_group_name = "${azurerm_resource_group.k8s.name}"
+  resource_group_name = "${azurerm_kubernetes_cluster.k8s_ingress.node_resource_group}"
   sku                 = "Standard"
   allocation_method   = "Static"
   domain_name_label   = "azk8terraform-${local.environment}"
